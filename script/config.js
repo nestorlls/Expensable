452d5991f34f533f153dba389a2c8b7384f1ec49
Paul Llanque <llanquepol@gmail.com>
2023-02-16 15:43:34 -0500

    update config file

--- script/config.js
+++ script/config.js
@@ -1,7 +1,5 @@
 // base_uri
-const BASE_URI = 'https://expensable-api.herokuapp.com/';
+export const BASE_URI = 'https://expensable-api.herokuapp.com/';
 
-// token key in sessionStorage
-const tokenKey = 'expensable_key';
-
-export {BASE_URI, tokenKey}
+// get token
+export const tokenKey = 'expensanble_token';
