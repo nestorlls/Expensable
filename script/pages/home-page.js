42291bf1ea1f628ff75d2a0d05cece972d688253
Paul Llanque <llanquepol@gmail.com>
2023-02-16 13:35:03 -0500

    add Dom Handler

---
+++ script/pages/home-page.js
@@ -0,0 +1,7 @@
+const homePage = {
+  toString() {
+    return `<h1>Home Page!!</h1>`;
+  },
+  addListeners() {}
+};
+export default homePage;
---
+++ script/pages/login-page.js
@@ -0,0 +1,7 @@
+const loginPage = {
+  toString() {
+    return `<h1>Login Page!!</h1>`;
+  },
+  addListeners() {}
+};
+export default loginPage;
