2a6b510cb6aa75ee24b10aae48534ad268d51a3f
Paul Llanque <llanquepol@gmail.com>
2023-02-16 16:42:18 -0500

    add render and style nav

--- script/pages/home-page.js
+++ script/pages/home-page.js
@@ -1,7 +1,26 @@
-const homePage = {
+// imports
+import STORE from "../store.js";
+
+function render() {
+  console.log(STORE);
+  return `
+    <main class="section">
+      <section class="container">
+        <h1 class="heading heading--lg text-center mb-2">Expensable</h1>
+        <div class="flex justify-between mb-8 js-navigation">
+          <a href="#" class="button button--subtle ">Expense</a>
+          <a href="#" class="button button--subtle ">Income</a>
+          <a href="#" class="button button--subtle ">Profile</a>
+        </div>
+      </section>
+    </main>
+  `;
+}
+
+const HomePage = {
   toString() {
-    return `<h1>Home Page!!</h1>`;
+    return render();
   },
-  addListeners() {}
+  addListeners() {},
 };
-export default homePage;
+export default HomePage;
