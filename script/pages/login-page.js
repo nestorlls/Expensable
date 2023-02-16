4f69437c61cb246cc48859edf0933d7d0b4d14f9
Paul Llanque <llanquepol@gmail.com>
2023-02-16 15:49:43 -0500

    add render login page

--- script/pages/login-page.js
+++ script/pages/login-page.js
@@ -1,7 +1,38 @@
-const loginPage = {
+import { input } from '../components/input.js';
+
+function render() {
+  return `
+  <main class="section">
+    <section class="container">
+      <h1 class="heading--lg text-center mb-4">Login</h1>
+      <form class="flex flex-column gap-4 mb-4 js-login-form">
+        ${input({
+          label: 'email',
+          id: 'email',
+          placeholder: 'john@example.com',
+          type: 'email',
+          required: true,
+          value: 'test3@mail.com',
+        })}
+        ${input({
+          label: 'password',
+          id: 'password',
+          placeholder: '******',
+          type: 'password',
+          required: true,
+          value: 'invalid',
+        })}
+        <button class="button button--primary">Login</button>
+      </form>
+    <a href="#" class="block text-center js-signup-link">Create account</a>
+    </section>
+  </main>
+  `;
+}
+const LoginPage = {
   toString() {
-    return `<h1>Login Page!!</h1>`;
+    return render();
   },
-  addListeners() {}
+  addListeners() {},
 };
-export default loginPage;
+export default LoginPage;
