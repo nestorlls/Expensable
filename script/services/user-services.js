b49f15d3a07f9e9922a3e3de72cc4a6bb5ca1903
Paul Llanque <llanquepol@gmail.com>
2023-02-16 15:42:39 -0500

    add getUser method

--- script/services/user-services.js
+++ script/services/user-services.js
@@ -1,20 +1,29 @@
+//imports
 import { tokenKey } from '../config.js';
 import apiFetch from './api-fetch.js';
 
+// create user
 export async function createUser(
-  newUser = { email, password, firstName, lastName, phone }
+  newUser = { email, password, first_name, last_name, phone }
 ) {
-  const { token, ...user } = await apiFetch('singup', { body: newUser });
+  const { token, ...user } = await apiFetch('signup', { body: newUser });
   sessionStorage.setItem(tokenKey, token);
   return user;
 }
 
+// update User
 export async function updateUser(
-  data = { email, password, firstName, lastName, phone }
+  data = { email, first_name, last_name, phone }
 ) {
-  const { token, ...user } = await apiFetchtch('profile', {
+  const { token, ...user } = await apiFetch('profile', {
     body: data,
     method: 'PATCH',
   });
+  return user;
+}
+
+// get user
+export async function getUser() {
+  const { token, ...user } = await apiFetch('profile');
   return user;
 }
